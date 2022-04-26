1da9158d2dd4a7e11fae332768962acabfee9049
SamaraSilvia81 <samarasilvia3110@gmail.com>
2022-04-26 14:36:58 -0300

    update

--- aula11/ex012/index.js
+++ aula11/ex012/index.js
@@ -1,24 +1,26 @@
-const inputNacional = document.querySelector("input#txtN").value.toLoweCase();
+const inputNacional = document.querySelector("input#txtN")
 const btVerificar = document.getElementById("verifica")
 const btLimpar = document.getElementById("limpar")
 const res = document.querySelector("div#res")
 
 function analise(){
 
-    nacional = inputNacional.value;
+    let nacional = inputNacional.value
 
-    inputNacional.value = " " // Após ter sido executado a soma os inputs se limparão.
+    inputNacional.value = " " // Após ter sido executado a análise os inputs se limparão.
     
+    res.innerHTML = `<p>Você está vivendo no(a) <strong>${nacional}<strong>.</p>`
+
     if(nacional == "Brasil"){
-        res.innerHTML += `<p>Você é <strong>Brasileiro</strong></p>`
+        res.innerHTML += `<p>Você é <strong>Brasileiro</strong>.</p>`
     } else{
-        res.innerHTML += `<p>Você é <strong>Estrangeiro</strong></p>`
+        res.innerHTML += `<p>Você é <strong>Estrangeiro</strong>.</p>`
     }
 
 }
 
 function remove(){
-    res.innerHTML = " ";
+    res.innerHTML = " "
 }
 
 verifica.onclick = analise
